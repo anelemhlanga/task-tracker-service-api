1670211956f699a64378eff2d6ed971d3aa09963
Anele Mhlanga <anelenmhlanga@gmail.com>
2022-12-24 08:49:22 +0200

    add sqlite3 db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'task.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
