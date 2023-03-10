0d055865012e5f0d3d0ef9c09d472fbe623c115c
Anele Mhlanga <anelenmhlanga@gmail.com>
2023-03-10 23:25:14 +0200

    Update task.controller.ts

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { CreateTaskDto } from './create-task.dto';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+
+  constructor(){}
+  
+  createTask(@Body() body : CreateTaskDto()){
+    console.log(body);
+    
+  }
+}
